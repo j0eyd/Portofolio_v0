19d567fb9acc55368dd59af945f755098f0fa5cc
Joey <joeydavid99@gmail.com>
2024-01-14 17:56:44 -0800

    Nearly finalized template

--- script.js
+++ script.js
@@ -7,5 +7,14 @@ function toggleMenu() {
 
 function sendEmail() 
 {
-    window.location = "mailto:joeydavid99@gmail.com";
+    window.location = "mailto:joeydavid99@gmail.com";}
+
+function copyToClipboard(textToCopy, Name) {
+    navigator.clipboard.writeText(textToCopy)
+    .then(() => {	
+    	alert(Name + " copied to clipboard!");
+    })
+    .catch(err => {
+		console.error("Unable to copy text to clipboard", err);
+	});
 }
